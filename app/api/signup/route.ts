fa07d92e816ba80ef48b45f6f8bca9aaa02627b5
Robert Von Der Becke <teamvcorp@thevacorp.com>
2025-07-24 14:22:19 -0500

    bcrypt import

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { connectToDatabase } from '@/lib/mongodb';
 import { UserModel } from '@/models/User';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export async function POST(req: Request) {
   const { name, email, password } = await req.json();
